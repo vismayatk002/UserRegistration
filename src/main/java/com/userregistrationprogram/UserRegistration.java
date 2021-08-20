979de00c1ee510d9eaef58b76cbc4ad482e4879f
vismayatk002 <vismayatk002@gmail.com>
2021-08-20 23:36:20 +0530

    Updated with throw custom exception for invalid user details

--- src/main/java/com/userregistrationprogram/UserRegistration.java
+++ src/main/java/com/userregistrationprogram/UserRegistration.java
@@ -3,103 +3,89 @@ package com.userregistrationprogram;
 import java.util.*;
 import java.util.regex.Pattern;
 
+
 public class UserRegistration {
 	
-	public boolean validateName(String Name) {
+	public void validateName(String Name) throws UserRegistrationInvalidException{
 			
-		boolean returnFlag = true;
 		if(Character.isUpperCase(Name.charAt(0))) {
 			
 			if(Name.length() < 3) {
 				
-				System.out.println( "First name should have minimum 3 letters");
-				returnFlag = false;
+				throw new UserRegistrationInvalidException("First name should have minimum 3 letters");
 			}
 		}
 		else {
-			System.out.println( "first letter must be caps");
-			returnFlag = false;
+			throw new UserRegistrationInvalidException( "first letter must be caps");
 		}
-		return returnFlag;
 	}
 	
-	public boolean validateEmail(String email) {
+	public void validateEmail(String email) throws UserRegistrationInvalidException{
 			
 		String regex = "^(.+)@bl.co(.+)$"; 
 		Pattern pattern = Pattern.compile(regex); 
-		if(pattern.matcher(email).matches()) {
-			return true;
-		}
-		else {
-			System.out.println( "Invalid Email id");
-			return false;
+		if(!pattern.matcher(email).matches()) {
+			
+			throw new UserRegistrationInvalidException( "Invalid Email id");
 		}
 	}
 	
-	public boolean validateMobileNo(String mobNo) {
+	public void validateMobileNo(String mobNo) throws UserRegistrationInvalidException{
 		
 		Pattern pattern = Pattern.compile("91[0-9]{10}");
-		if(pattern.matcher(mobNo).matches()) {
-			return true;
-		}
-		else {
-			System.out.println( "Invalid Mobile number");
-			return false;
+		if(!pattern.matcher(mobNo).matches()) {
+			throw new UserRegistrationInvalidException( "Invalid Mobile number");
 		}
 	}
 	
-	public boolean validatePassword(String password) {
+	public void validatePassword(String password) throws UserRegistrationInvalidException{
 		
-		boolean returnFlag = true;
 		Pattern UpperCasePatten = Pattern.compile("[A-Z]");
 		Pattern numericNumberPatten = Pattern.compile("[0-9 ]");
 		Pattern specialCharacterPatten = Pattern.compile("[^a-zA-Z0-9]");
 		if(password.length() < 8) {
 			
-			System.out.println( "Password should have minimum 8 characters");
-			returnFlag = false;
+			throw new UserRegistrationInvalidException( "Password should have minimum 8 characters");
 		}
 		else if (!UpperCasePatten.matcher(password).find()){
 		
-			System.out.println("Password must have atleast one uppercase character");
-			returnFlag=false;
+			throw new UserRegistrationInvalidException("Password must have atleast one uppercase character");
 		}
 		else if (!numericNumberPatten.matcher(password).find()){
 			
-			System.out.println("Password must have atleast one numeric number");
-			returnFlag=false;
+			throw new UserRegistrationInvalidException("Password must have atleast one numeric number");
 		}
 		else if (!specialCharacterPatten.matcher(password).find()){
 			
-			System.out.println("Password must have exactly one special character");
-			returnFlag=false;
+			throw new UserRegistrationInvalidException("Password must have exactly one special character");
 		}
-		return returnFlag;
 	}
 	
     public static void main( String[] args ) {
         
-    	UserRegistration user = new UserRegistration();
-    	Scanner sc = new Scanner(System.in);	
-    	System.out.print( "Enter your first name : ");
-    	String firstName = sc.nextLine(); 
-    	if(user.validateName(firstName)) {
-    		System.out.print( "Enter your last name : ");
-    		String lastName = sc.nextLine();
-    		if(user.validateName(lastName)) {
-    			System.out.print( "Enter your email id : ");
-        		String email = sc.nextLine();
-        		if(user.validateEmail(email)) {
-		    		System.out.print( "Enter your Mobile Number : ");
-		    		String mobNo = sc.nextLine(); 
-		    		if(user.validateMobileNo(mobNo)) {
-			    		System.out.print( "Enter your Password : ");
-			    		String password = sc.nextLine(); 
-			    		user.validatePassword(password);
-		    		}
-        		}
-    		}
+    	try {
+    		
+	    	UserRegistration user = new UserRegistration();
+	    	Scanner sc = new Scanner(System.in);	
+	    	System.out.print( "Enter your first name : ");
+	    	String firstName = sc.nextLine(); 
+	    	user.validateName(firstName);
+			System.out.print( "Enter your last name : ");
+			String lastName = sc.nextLine();
+			user.validateName(lastName);
+			System.out.print( "Enter your email id : ");
+			String email = sc.nextLine();
+			user.validateEmail(email);
+			System.out.print( "Enter your Mobile Number : ");
+			String mobNo = sc.nextLine(); 
+			user.validateMobileNo(mobNo);
+			System.out.print( "Enter your Password : ");
+			String password = sc.nextLine(); 
+			user.validatePassword(password);
+			
+    	}catch(UserRegistrationInvalidException e) {
+    		
+    		System.out.println(e.getMessage());
     	}
-    	sc.close();
     }
 }
---
+++ src/main/java/com/userregistrationprogram/UserRegistrationInvalidException.java
@@ -0,0 +1,8 @@
+package com.userregistrationprogram;
+
+public class UserRegistrationInvalidException extends Exception{
+
+	public UserRegistrationInvalidException(String Message) {
+		super(Message);
+	}
+}
