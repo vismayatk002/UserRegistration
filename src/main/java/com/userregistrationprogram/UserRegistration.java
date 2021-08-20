866937a4529f89c2deadcda19a864de8dd244132
vismayatk002 <vismayatk002@gmail.com>
2021-08-20 09:52:50 +0530

    Added welcome message

---
+++ src/main/java/com/userregistrationprogram/UserRegistration.java
@@ -0,0 +1,9 @@
+package com.userregistrationprogram;
+
+public class UserRegistration 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to User Registration Program" );
+    }
+}
